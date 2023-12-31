7b53ef3eb4d68f02081809ee311e9a539c304b0f
AADITYAPRABALCHAWLA <103323016+AADITYAPRABALCHAWLA@users.noreply.github.com>
2023-12-31 23:30:48 +0530

    Update RCode.R

--- RCode.R
+++ RCode.R
@@ -31,3 +31,45 @@ library(data.table)
  oct2022=read.csv("202210-divvy-tripdata.csv")
  nov2022=read.csv("202211-divvy-tripdata.csv")
  dec2022=read.csv("202212-divvy-tripdata.csv")
+
+# merging data yearwise
+cycle2021=rbind(jan2021,feb2021,mar2021,apr2021,may2021,jun2021,jul2021,aug2021,sep2021,oct2021,nov2021,dec2021)
+cycle2022=rbind(jan2022,feb2022,mar2022,apr2022,may2022,jun2022,jul2022,aug2022,sep2022,oct2022,nov2022,dec2022)
+
+# creating new data frames to add more columns
+cycle2021_date=cycle2021
+cycle2022_date=cycle2022
+
+# calculating ride length
+cycle2021_date$ride_length=difftime(cycle2021$ended_at,cycle2021$started_at,units="mins")
+cycle2022_date$ride_length=difftime(cycle2022$ended_at,cycle2022$started_at,units="mins")
+
+# creating columns for date,day of week, month,day,year,time and hour
+cycle2021_date$date=as.Date(cycle2021$started_at)
+cycle2022_date$date=as.Date(cycle2022$started_at)
+cycle2021_date$days_of_week=wday(cycle2021$started_at)
+cycle2022_date$days_of_week=wday(cycle2022$started_at)
+cycle2021_date$month=foramt(as.Date(cycle2021$date),"m")
+cycle2021_date$days_of_week=format(as.Date(cycle2021_date$date),"%A")
+cycle2022_date$days_of_week=format(as.Date(cycle2022_date$date),"%A")
+cycle2021_date$month=format(as.Date(cycle2021_date$date),"%m")
+cycle2022_date$month=format(as.Date(cycle2022_date$date),"%m")
+cycle2022_date$day=format(as.Date(cycle2021_date$date),"%d")
+cycle2021_date$day=format(as.Date(cycle2021_date$date),"%d")
+cycle2022_date$day=format(as.Date(cycle2022_date$date),"%d")
+cycle2021_date$day=format(as.Date(cycle2021_date$date),"%y")
+cycle2022_date$day=format(as.Date(cycle2022_date$date),"%y")
+cycle2021_date$time=format(as.Date(cycle2022_date$date),"%h:%m:%s")
+cycle2021_date$hour=hour(cycle2021_date$time)
+
+
+
+
+
+
+ 
+
+
+
+
+
